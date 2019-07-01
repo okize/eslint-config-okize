1be1b86e8032a9527d248a766f2a742d2ec73715
Morgan Wigmanich <mwigmanich@wistia.com>
2019-07-01 07:00:00 -0400

    add react ruleset

---
+++ rules/react.js
@@ -0,0 +1,26 @@
+// only add react rules
+
+module.exports = {
+  // it's acceptable to mix these even at the expense of consistency
+  'react/destructuring-assignment': 'off',
+  // it's useful to be able to reuse prop types off of components
+  // since we don't remove prop types in production it's safe to disable this rule
+  'react/forbid-foreign-prop-types': 'off',
+  // in theory it would be good to enforce but in practice we sometimes need to be vague
+  'react/forbid-prop-types': 'off',
+  // force consistency of jsx attribute declaration
+  'react/jsx-boolean-value': ['error', 'always'],
+  // this rule is overreaching in it's prescriptiveness
+  'react/jsx-one-expression-per-line': 'off',
+  // it's good to avoid but there are occasions where you need it and will just end up disabling this rule inline anyways
+  'react/no-danger': 'off',
+  // disabled because you end up writing a lot of { className: '' } default props
+  'react/require-default-props': 'off',
+  // alpha-sort prop types for lack of a better idea
+  'react/sort-prop-types': ['error', {
+    'sortShapeProp': true,
+    'ignoreCase': true,
+  }],
+  // alpha-sort default props for consistency
+  'react/jsx-sort-default-props': ['error'],
+};
