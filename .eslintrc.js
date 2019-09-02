5e275071f1d4e017328b3ef1e4a4056d1ff3545a
Morgan Wigmanich <okize123@gmail.com>
2019-09-02 07:00:00 -0400

    add prettier to eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,8 @@
 module.exports = {
   root: true,
-  extends: './base.js',
-}
+  extends: ['./base.js', 'prettier'],
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+  },
+};
